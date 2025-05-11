bac6f76df318ffbc069eda93bb625f26b72e701c
pionpill <673486387@qq.com>
2025-05-11 23:16:29 +0800

    :art: 格式化土地文件

--- src/land/data.ts
+++ src/land/data.ts
@@ -1,56 +1,56 @@
 import type { LandData } from "./type";
 
 const LAND_DATA: Array<LandData> = [
-    {
-        blockId: "minecraft:farmland",
-        fertility: 120,
-        tags: ["dirt"],
-        state: {
-            fertility: 130,
-            attribute: 'moisture',
-            value: 7,
-        },
+  {
+    blockId: "minecraft:farmland",
+    fertility: 120,
+    tags: ["dirt"],
+    state: {
+      fertility: 130,
+      attribute: "moisture",
+      value: 7,
     },
-    {
-        blockId: "minecraft:podzol",
-        fertility: 100,
-        tags: ["dirt", "fungi"],
-    },
-    {
-        blockId: "minecraft:grass",
-        fertility: 90,
-        tags: ["dirt"],
-    },
-    {
-        blockId: "minecraft:grass_path",
-        fertility: 70,
-        tags: ["dirt"],
-    },
-    {
-        blockId: "minecraft:dirt",
-        fertility: 80,
-        tags: ["dirt"],
-    },
-    {
-        blockId: "minecraft:sand",
-        fertility: 40,
-        tags: ["sand"],
-    },
-    {
-        blockId: "minecraft:red_sand",
-        fertility: 30,
-        tags: ["sand"],
-    },
-    {
-        blockId: "minecraft:mycelium",
-        fertility: 130,
-        tags: ["fungi"],
-    },
-    {
-        blockId: "minecraft:moss_block",
-        fertility: 90,
-        tags: ["dirt"],
-    },
-];
+  },
+  {
+    blockId: "minecraft:podzol",
+    fertility: 100,
+    tags: ["dirt", "fungi"],
+  },
+  {
+    blockId: "minecraft:grass",
+    fertility: 90,
+    tags: ["dirt"],
+  },
+  {
+    blockId: "minecraft:grass_path",
+    fertility: 70,
+    tags: ["dirt"],
+  },
+  {
+    blockId: "minecraft:dirt",
+    fertility: 80,
+    tags: ["dirt"],
+  },
+  {
+    blockId: "minecraft:sand",
+    fertility: 40,
+    tags: ["sand"],
+  },
+  {
+    blockId: "minecraft:red_sand",
+    fertility: 30,
+    tags: ["sand"],
+  },
+  {
+    blockId: "minecraft:mycelium",
+    fertility: 130,
+    tags: ["fungi"],
+  },
+  {
+    blockId: "minecraft:moss_block",
+    fertility: 90,
+    tags: ["dirt"],
+  },
+] as const;
 
 export default LAND_DATA;
